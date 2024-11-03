66260848d9afadf4b40fae5515855ac4a9fe07ad
imnaK <mc.yama1000@gmail.com>
2024-11-03 20:40:56 +0100

    feat: add coin flip on click

--- src/components/CoinFlip.vue
+++ src/components/CoinFlip.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  const coinRef = ref<HTMLElement>(null);
+  const coinSide = ref<boolean>(false);
+
+  function flipCoin() {
+    coinSide.value = Math.floor(Math.random() * 2) === 1;
+    coinRef.value.textContent = coinSide.value ? "Heads" : "Tails";
+  }
+</script>
 
 <template>
-  <div class="coin-flip">C</div>
+  <div ref="coinRef" @click="flipCoin" class="coin-flip">C</div>
 </template>
 
 <style lang="scss">
   .coin-flip {
+    cursor: pointer;
+    user-select: none;
     width: 8rem;
     height: 8rem;
     padding: 2rem;
