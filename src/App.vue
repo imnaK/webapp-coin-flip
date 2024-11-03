26bab0787f7fea588a62ad22774dfbb5eea2e9e7
imnaK <mc.yama1000@gmail.com>
2024-11-03 20:32:17 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import CoinFlip from "@/components/CoinFlip.vue";
+</script>
+
+<template>
+  <div>
+    <a href="https://security.tyrem-envalura.de" target="_blank">
+      <img src="/logo.svg" class="logo" alt="Tyrem Envalura Logo" />
+    </a>
+  </div>
+  <CoinFlip />
+</template>
+
+<style scoped>
+  .logo {
+    height: 8rem;
+    padding: 2rem;
+    will-change: filter;
+    transition: filter 0.3s;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #ff07);
+  }
+</style>
---
+++ src/components/CoinFlip.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="coin-flip">C</div>
+</template>
+
+<style lang="scss">
+  .coin-flip {
+    width: 8rem;
+    height: 8rem;
+    padding: 2rem;
+    border-radius: 4rem;
+    background-color: #f70;
+    font-size: 4rem;
+    line-height: 1;
+    text-align: center;
+  }
+</style>
